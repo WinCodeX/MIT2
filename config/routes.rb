7851a9ce58f9834aa7594a53910056be36d9e5b0
WinCodeX <glenwinterg970@gmail.com>
2023-02-17 16:55:18 +0300

    logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
-  get "home", to:  "pages#home"
-  
+  get "student", to: "students#student"
+  root "pages#home"
+ get "show this student", to: "students#show" 
   resources :students
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
 end
+
