dd9b7c1fc66fc3e5d4389bd3dc9907f76b5b7a92
WinCodeX <glenwinterg970@gmail.com>
2023-02-17 10:26:10 +0300

    new students

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def contact
+  end
+
+  def about
+    @title = "My Cool Page"
+  end
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "contact", to: "pages#contact"
+  get "about", to: "pages#about"
+  get "home", to:  "pages#home"
+  
   resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
